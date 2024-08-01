a8562544b64c5245c42167c539d3da0b3d764ae8
mallikarjun <mallijangam@gmail.com>
2024-08-01 12:28:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const {open} = require('sqlite');
+const sqlite3 = require('sqlite3');
+const app = express();
+
+const dbPath = path.join(__dirname,"user.db");
+
+let db = null;
+
+const initilizeDbAndServer = async() => {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        });
+        app.listen(5005, () => {
+            console.log('Server Running at 5005');
+        })
+        
+    } catch (error) {
+        console.log(`Db Error: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+
+
+initilizeDbAndServer();
